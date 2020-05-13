1efdcbd5b56d906af50dc98853d004b552e42d4a
Ayoola David <ayooladavid@gmail.com>
2020-05-14 00:59:59 +0100

    developments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import List from './containers/List'
 
 const App = () => {
-	return <h1>MovieList</h1>
+	return <List />
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
