67fee942ec624d6ee8c4be4c13777f52f2beb587
Ayoola David <ayooladavid@gmail.com>
2020-05-14 06:31:20 +0100

    development

--- src/containers/List.js
+++ src/containers/List.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react-dom'
 import Card from './components/Card/Card'
 
 class List extends Component {
+    
+    constructor(){
+    	super()
+    	this.state = {
+    		data : {},
+    		loading: true
+    	}
+    }
+
     render() {
         return < Card />
     }
