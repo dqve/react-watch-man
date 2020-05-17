c6be589f3700f3f8d462559aa7aea74a2c4c1037
Ayoola David <ayooladavid@gmail.com>
2020-05-17 12:42:14 +0100

    List edits

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react-dom'
-import Card from './components/Card/Card'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Card from '../components/Card/Card'
 
 class List extends Component {
     
@@ -7,12 +8,25 @@ class List extends Component {
     	super()
     	this.state = {
     		data : {},
-    		loading: true
+    		loading: true,
     	}
     }
+
+    async componentDidMount() {
+        const movies = await fetch('../../assets/data.json')
+        const moviesJSON = await movies.json()
+        if (moviesJSON) {
+            this.setState({
+                data: moviesJSON,
+                loading: false,
+            });
+        }
+    }
+
 
     render() {
         return < Card />
     }
-};
+}
+
 export default List
