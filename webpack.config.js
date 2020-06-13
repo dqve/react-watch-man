eeb87868aef6a95ddab5d87c1cd470fcba17b713
Ayoola David <ayooladavid@gmail.com>
2020-06-13 06:42:05 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,7 @@ module.exports = {
         rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                }
+            use: ['babel-loader', 'eslint-loader']
             },
             {
                 test: /\.css$/,
