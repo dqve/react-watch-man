f40eb55d65e24d071c4a9339f6f074baeed1cae9
Ayoola David <ayooladavid@gmail.com>
2020-06-13 06:01:29 +0100

    update webpack rules

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,13 @@ module.exports = {
             exclude: /node_modules/,
             use: {
                 loader: 'babel-loader',
+                }
             },
-        }, ],
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+         ],
     },
     plugins: [htmlPlugin],
 };
