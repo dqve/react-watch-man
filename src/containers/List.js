f620ef0d631f3245acb5ef1b65e21a6dd8683389
Ayoola David <ayooladavid@gmail.com>
2020-06-13 05:31:21 +0100

    remove redundant files

--- src/containers/List.js
+++ src/containers/List.js
@@ -25,7 +25,18 @@ class List extends Component {
 
 
     render() {
-        return < Card />
+            const {
+                data,
+                loading
+            } = this.state;
+            if (loading) {
+                return <div> Loading... </div>
+            }
+            return data.map(movie => < Card key = {
+                        movie.id
+                    }
+                    movie = { movie }
+                    />);
     }
 }
 
